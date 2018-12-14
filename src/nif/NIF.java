2e9112ad823eb1f198b9a00318a5bb37e0e3dec1
ik013043z1 <ik013043z1@hz20607.H013043.NET>
2018-12-14 13:36:27 +0100

    añadir supuesto con switch

--- src/nif/NIF.java
+++ src/nif/NIF.java
@@ -62,9 +62,99 @@ public class NIF {
 			letra='K';
 		}else if(division==22) {
 			letra='E';
+		}else {
+			System.out.println("algo raro ha pasado "+division);
 		}
 		return letra;
 	}
+	
+	
+	/*
+	 * public char leer(long DNI) {
+		Scanner scan = new Scanner(System.in);
+		int division=0;
+		System.out.println("introduce el numero de dni");
+		DNI=scan.nextLong();
+		division=(int)DNI%23;
+		
+		switch (division){
+		
+		case 0:
+			letra='T';
+			break;
+		case 1:
+			letra='R';
+			break;
+		case 2:
+			letra='W';
+			break;
+		case 3:
+			letra='A';
+			break;
+		case 4:
+			letra='G';
+			break;
+		case 5:
+			letra='M';
+			break;
+		case 6:
+			letra='Y';
+			break;
+		case 7:
+			letra='F';
+			break;
+		case 8:
+			letra='P';
+			break;
+		case 9:
+			letra='D';
+			break;
+		case 10:
+			letra='X';
+			break;
+		case 11:
+			letra='B';
+			break;
+		case 12:
+			letra='N';
+			break;
+		case 13:
+			letra='J';
+			break;
+		case 14:
+			letra='Z';
+			break;
+		case 15:
+			letra='S';
+			break;
+		case 16:
+			letra='Q';
+			break;
+		case 17:
+			letra='V';
+			break;
+		case 18:
+			letra='H';
+			break;
+		case 19:
+			letra='L';
+			break;
+		case 20:
+			letra='C';
+			break;
+		case 21:
+			letra='K';
+			break;
+		case 22:
+			letra='E';
+			break;
+		default:
+			System.out.println("algo raro ha pasado "+division);
+		}
+		
+		return letra;
+	}
+	 */
 	
 	public String toString() {
 		return "El DNI con letra es "+DNI+"-"+letra;
